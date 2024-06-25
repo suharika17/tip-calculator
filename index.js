14e567f78034dcc950dfc90bf3cec4edf88aada3
Suharika Talluri <tallurisuharika@gmail.com>
2024-06-25 12:57:29 +0530

    application finished

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ Array(...tipButtons).map((tipButton) => {
         tipPercentage = event.target.textContent.slice(0, event.target.textContent.length - 1)
         tipPercentage = parseInt(tipPercentage)
         tipAmount = document.getElementById("tipAmount")
-        tipAmount.textContent = billValue * (tipPercentage / 100)
+        tipAmount.textContent = `$${billValue * (tipPercentage / 100)}`
+        console.log(tipAmount.textContent)
         totalContainer = document.getElementById("totalContainer")
-        totalContainer.textContent = billValue + parseInt(tipAmount.textContent)
+        totalContainer.textContent = `$${billValue + parseInt(tipAmount.textContent.slice(1))}`
     })
 })
-
 const customInput = document.getElementById("customInput")
 customInput.addEventListener('change', (event) => {
     tipPercentage = event.target.value
     tipPercentage = parseInt(tipPercentage)
     console.log(tipPercentage)
     tipAmount = document.getElementById("tipAmount")
-    tipAmount.textContent = billValue * (tipPercentage / 100)
+    tipAmount.textContent = `$${billValue * (tipPercentage / 100)}`
     totalContainer = document.getElementById("totalContainer")
-    totalContainer.textContent = billValue + parseInt(tipAmount.textContent)
+    totalContainer.textContent = `$${billValue + parseInt(tipAmount.textContent.slice(1))}`
 })
