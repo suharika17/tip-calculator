ab48bcfdcdfff9c697cda7e613f841748122bb59
Suharika Talluri <tallurisuharika@gmail.com>
2024-06-25 12:35:54 +0530

    Tip Calculator

---
+++ index.js
@@ -0,0 +1,33 @@
+const bill = document.getElementById("bill")
+let billValue;
+let tipPercentage;
+let tipAmount;
+let totalContainer;
+
+bill.addEventListener('change', (event) => {
+    billValue = parseInt(event.target.value)
+})
+
+
+const tipButtons = document.querySelectorAll(".tip-buttons-container button")
+Array(...tipButtons).map((tipButton) => {
+    tipButton.addEventListener('click', (event) => {
+        tipPercentage = event.target.textContent.slice(0, event.target.textContent.length - 1)
+        tipPercentage = parseInt(tipPercentage)
+        tipAmount = document.getElementById("tipAmount")
+        tipAmount.textContent = billValue * (tipPercentage / 100)
+        totalContainer = document.getElementById("totalContainer")
+        totalContainer.textContent = billValue + parseInt(tipAmount.textContent)
+    })
+})
+
+const customInput = document.getElementById("customInput")
+customInput.addEventListener('change', (event) => {
+    tipPercentage = event.target.value
+    tipPercentage = parseInt(tipPercentage)
+    console.log(tipPercentage)
+    tipAmount = document.getElementById("tipAmount")
+    tipAmount.textContent = billValue * (tipPercentage / 100)
+    totalContainer = document.getElementById("totalContainer")
+    totalContainer.textContent = billValue + parseInt(tipAmount.textContent)
+})
